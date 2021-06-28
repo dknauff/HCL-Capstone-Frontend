1c524f16996ee6132d29ddafb08c73ac3f7cee35
Akinyemi Fayankinnu <akinyemi.fayankinnu@hcl.com>
2021-06-28 15:45:39 -0500

    Made changes to the homepage

--- src/components/MainPageJumbo.js
+++ src/components/MainPageJumbo.js
@@ -2,16 +2,16 @@ const MainPageJumbo = () => {
   return (
     <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 offset-3 float-md-center">
       <div className="jumbotron">
-        <h2 className="display-4">Shop Name</h2>
-        <p className="lead">Your one-stop-shop music shop</p>
-        <hr className="my-4" />
+        <h2 className="display-2">Hi-5</h2>
+        <p className="lead">Your one-stop-shop for musical instruments</p>
+        <hr className="my-6" />
         <p>
           {" "}
-          Looking for your next musical breakthrough? You've come to the right
-          place!
+          Looking to create your own musical masterpiece? You've come to the
+          right place!
         </p>
         <button className="btn btn-primary btn-lg" role="button">
-          Shop By Category
+          Start Shopping
         </button>
       </div>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,32 +6,56 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
-
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 const NavBar = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Music Shop</Navbar.Brand>
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Thing1</Nav.Link>
-            <Nav.Link href="#link">Thing2</Nav.Link>
-            <NavDropdown title="More Things" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Thing3</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Thing4</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Thing5</NavDropdown.Item>
+            <NavDropdown title="Shop By Department" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Drums and Percussion <NavDropdown.Divider />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Guitars <NavDropdown.Divider />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Amplifiers <NavDropdown.Divider />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Keyboards <NavDropdown.Divider />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">
+                Microphones
+                <NavDropdown.Divider />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.6">
+                Accessories
+                <NavDropdown.Divider />
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.7">Recording</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Thing6</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.8">Media</NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#home">Sale</Nav.Link>
           </Nav>
           <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
+            <FormControl
+              type="text"
+              placeholder="Enter keyword or item #"
+              className="mr-sm-1"
+            />
+            <Button variant="success">
+              <AiOutlineSearch></AiOutlineSearch>
+            </Button>
           </Form>
           <br />
-          <Button variant="outline-danger" style={{ float: "left" }}>
-            Cart
+          <Button variant="danger" style={{ float: "left" }}>
+            <AiOutlineShoppingCart />
           </Button>
         </Navbar.Collapse>
       </Navbar>
