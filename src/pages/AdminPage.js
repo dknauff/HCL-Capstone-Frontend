a3f2545a76a0d7bc529712e37beae263852a90ac
Danny Knauff <daniel.knauff@hcl.com>
2021-07-07 12:09:23 -0500

    admin styling

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -49,19 +49,31 @@ const AdminPage = () => {
           <h2>Admin Controls</h2>
         </Row>
         <Row className="justify-content-sm-center">
-          <h2>Category</h2>
-          <AddCategory />
-          <DeleteCategory />
-          <UpdateCategory />
+          <h2 style={{ paddingRight: "25px" }}>Category</h2>
+          <div style={{ paddingRight: "25px" }}>
+            <AddCategory />
+          </div>
+          <div style={{ paddingRight: "25px" }}>
+            <UpdateCategory />
+          </div>
+          <div style={{ paddingRight: "25px" }}>
+            <DeleteCategory />
+          </div>
         </Row>
         <Row className="justify-content-sm-center">
-          <h2>Product</h2>
-          <AddProduct />
-          <DeleteProduct />
-          <UpdateProduct />
+          <h2 style={{ paddingRight: "25px" }}>Product</h2>
+          <div style={{ paddingRight: "25px" }}>
+            <AddProduct />
+          </div>
+          <div style={{ paddingRight: "25px" }}>
+            <UpdateProduct />
+          </div>
+          <div style={{ paddingRight: "25px" }}>
+            <DeleteProduct />
+          </div>
         </Row>
         <Row className="justify-content-sm-center">
-          <h2>Order</h2>
+          <h2 style={{ paddingRight: "25px" }}>Order</h2>
           <UpdateOrder />
         </Row>
       </Container>
